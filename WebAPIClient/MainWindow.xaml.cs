aab559d8cf4f8e79bd6749ba87a21c06d931e1e6
ManikanthaN <32667168+ManikanthaN@users.noreply.github.com>
2017-10-10 14:41:04 +0530

    Devops test

---
+++ WebAPIClient/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebAPIClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:56851/");
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/User").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var users = response.Content.ReadAsAsync<IEnumerable<Users>>().Result;
+                
+                usergrid.ItemsSource = users;
+
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:56851/");
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var user = new Users();
+
+            user.FirstName = txtFirst.Text;
+            user.LastName = txtLas.Text;
+            user.Email = txtEmail.Text;
+            user.PhoneNo = txtPhone.Text;
+            user.Email = txtEmail.Text;
+            user.Company = txtCompany.Text;
+
+            var response = client.PostAsJsonAsync("api/User", user).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("User Added");
+                txtFirst.Text = "";
+                txtLas.Text = "";
+                txtPhone.Text = "";
+                txtEmail.Text = "";
+                txtCompany.Text = "";
+                GetData();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:56851/");
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var id = txtSearch.Text.Trim();
+
+            var url = "api/User/" + id;
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var users = response.Content.ReadAsAsync<Users>().Result;
+
+                MessageBox.Show("User Found : " + users.FirstName + " "  + users.LastName);
+
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:56851/");
+
+            var id = txtDelete.Text.Trim();
+
+            var url = "api/User/" + id;
+
+            HttpResponseMessage response = client.DeleteAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                
+
+                MessageBox.Show("User Deleted");
+                GetData();
+
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+    }
+}
